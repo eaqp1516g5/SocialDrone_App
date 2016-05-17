6be34466e4e8ec66acb1106b64d29f63033611aa
marcb10 <marc.ballesteros@hotmail.com>
2016-05-17 19:31:08 +0200

    camera

--- www/js/Controllers/SignupCtrl.js
+++ www/js/Controllers/SignupCtrl.js
@@ -2,11 +2,31 @@
  * Created by bernat on 17/04/16.
  */
 
-angular.module('starter').controller('SignupCtrl', function($scope, $ionicModal, $ionicPopover, $timeout, $http,$ionicPopup, $state) {
-
-
+angular.module('starter').controller('SignupCtrl', function($scope, $ionicModal, $ionicPopover, $timeout, $http,$ionicPopup, $state, $cordovaCamera) {
+    $scope.picture;
     $scope.newUser = {};
-    
+    $scope.takePicture = function(options){
+        var options = {
+            quality: 80,
+            sourceType: 1
+        }
+        $cordovaCamera.getPicture(options).then(function(imageData){
+            $scope.picture=imageData;
+        }, function(err){
+            console.log(err);
+        })
+    }
+    $scope.getPicture = function(options){
+        var options = {
+            quality: 80,
+            sourceType: 0
+        }
+        $cordovaCamera.getPicture(options).then(function(imageData){
+            $scope.picture=imageData;
+        }, function(err){
+            console.log(err);
+        })
+    }
     $scope.crearUser = function () {
         console.log($scope.newUser);
         $http.post(base_url + '/users', {
@@ -14,7 +34,8 @@ angular.module('starter').controller('SignupCtrl', function($scope, $ionicModal,
             password: $scope.newUser.password,
             name: $scope.newUser.name,
             lastname: $scope.newUser.lastname,
-            mail: $scope.newUser.mail
+            mail: $scope.newUser.mail,
+            imageUrl: $scope.picture
         }).success(function (data) {
             $state.go('app.login');
         }).error(function (err) {
